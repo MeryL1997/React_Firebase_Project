6984f9aace501390dcee87cf8694f196a6382380
MeryL1997 <50187870+MeryL1997@users.noreply.github.com>
2019-08-13 06:49:47 -0500

    modificacion del proyecto parte2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import firebase from './components/conexion/firebase';
+
+//Import de componentes
+import Header from './components/Header';
+import AddLab from './components/AddLab';
+import Horarios from './components/Horarios';
+import AddHorario from './components/AddHorario';
+import Labs from './components/Labs';
+import EditarHorario from './components/EditarHorario';
+import EditarLab from './components/EditarLab';
+import DetalleHorario from './components/DetalleHorario';
+import DetalleLab from './components/DetalleLab';
 
 function App() {
+  //En el brazo de pruebas
+  
+  const [laboratorios, setLaboratorios] = useState([]);
+  const [horarios, setHorarios] = useState([]);
+  const [recargar, setRecargar] = useState(true);
+
+  useEffect(() => {
+    if (recargar) {
+      firebase.firestore().collection('laboratorio').onSnapshot((snapshot)=>{
+        const datos = snapshot.docs.map((dato)=>({
+          id: dato.id,
+          ...dato.data()
+        }))
+        setLaboratorios(datos);
+      });
+      firebase.firestore().collection('horario').onSnapshot((snapshot)=>{
+        const datos = snapshot.docs.map((dato)=>({
+          id: dato.id,
+          ...dato.data()
+        }))
+        setHorarios(datos);
+      });
+    }
+
+    //Cambiar a false la recarga, para que no se este consultando a la API a cada momento
+    setRecargar(false)
+    
+  }, [recargar])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <main className="container mb-5 ">
+        <Switch>
+          <Route exact path="/" render={()=>(
+            <Labs laboratorios={laboratorios} recargar={setRecargar}/>
+          )} />
+
+          <Route exact path="/nuevo-laboratorio"  render={()=>(
+            <AddLab recargar={setRecargar} />
+          )} />
+
+          <Route exact path="/horarios" render={()=>(
+            <Horarios horarios={horarios} recargar={setRecargar}/>
+          )} />
+
+          <Route exact path="/nuevo-horario" render={()=>(
+            <AddHorario datos={laboratorios} recargar={setRecargar} />
+          )} />
+
+          <Route exact path="/horarios/editar/:id" render={props=> {
+            //Tomando el id del horario
+            const idHorario = props.match.params.id;
+            //horario que se pasa al state 
+            const horario = horarios.filter(horario => horario.id === idHorario);
+            return (
+              <EditarHorario horario={horario[0]} datos={laboratorios} recargar={setRecargar} />
+            )
+          }}/>
+          <Route exact path="/laboratorios/editar/:id" render={props=>{
+            //Tomando el id del lab
+            const idLab = props.match.params.id;
+            //lab que se pasa al state 
+            const lab = laboratorios.filter(lab => lab.id === idLab);
+            return (
+              <EditarLab lab={lab[0]} recargar={setRecargar} />
+            )
+          }} />
+
+          <Route exact path="/horarios/detalle/:id" component={DetalleHorario} /> 
+
+          <Route exact path="/laboratorios/detalle/:id" component={DetalleLab}/>
+        </Switch>
+      </main>
+      <footer id="sticky-footer" className="py-4 bg-dark text-white-50">
+        <div className="container text-center">
+          <small>Copyright &copy; webtechq.com</small>
+        </div>
+      </footer>
+    </Router>
   );
 }
 
